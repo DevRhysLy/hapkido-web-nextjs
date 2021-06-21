7148147a25d9a4f5652d3f5c45a234f2890e1055
DevRhysLy <rhysproduction@gmail.com>
2021-06-21 20:31:19 +1000

    added some styles to testimonial box

--- components/Testimonials/Testimonial.js
+++ components/Testimonials/Testimonial.js
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
 import Carousel from 'react-multi-carousel';
 import "react-multi-carousel/lib/styles.css";
+import StarsRating from 'stars-rating';
+import Quote from "components/Typography/Quote.js";
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
 
 function Testimonial({ testimonials = [] }) {
     const sizing = {
         superLargeDesktop: {
             breakpoint: { max: 4000, min: 3000 },
-            items: 2
+            items: 1
         },
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 2
+            items: 1
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
-            items: 2
+            items: 1
         },
         mobile: {
             breakpoint: { max: 464, min: 0 },
@@ -24,33 +28,43 @@ function Testimonial({ testimonials = [] }) {
     return (
         <div>
             <Carousel
-            additionalTransfrom={0}
-            arrows
-            autoPlay
-            autoPlaySpeed={4000}
-            centerMode={false}
-            className=""
-            containerClass="container-with-dots"
-            dotListClass=""
-            draggable
-            focusOnSelect={false}
-            infinite
-            itemClass=""
-            keyBoardControl
-            minimumTouchDrag={80}
-            renderButtonGroupOutside={false}
-            renderDotsOutside={false}
+                additionalTransfrom={0}
+                arrows
+                autoPlay
+                autoPlaySpeed={4000}
+                centerMode={false}
+                className=""
+                containerClass="container-with-dots"
+                dotListClass=""
+                draggable
+                focusOnSelect={false}
+                itemClass=""
+                keyBoardControl
+                minimumTouchDrag={80}
+                renderButtonGroupOutside={false}
+                renderDotsOutside={false}
                 responsive={sizing}
                 showDots={false}
                 sliderClass=""
-                slidesToSlide={2}
+                slidesToSlide={1}
                 swipeable>
                 {testimonials.map((testimonial) => (
                     <div key={testimonial.sys.id}>
                         <div>
-                            <h4>{testimonial.fields.name}</h4>
-                            <p>{testimonial.fields.testimonialDescription}</p>
-                            <div>{testimonial.fields.rating}</div>
+                            <Card>
+                                <CardBody>
+                                    <Quote
+                                        text={testimonial.fields.testimonialDescription}
+                                        author={testimonial.fields.name}
+                                    />
+                                    <StarsRating
+                                        count={5}
+                                        value={testimonial.fields.rating}
+                                        size={24}
+                                        color2={'#ffd700'}
+                                        edit={false} />
+                                </CardBody>
+                            </Card>
                         </div>
                     </div>
                 ))}

ed89679e7ae7f8a40447a899fcaa85b0ccb8f221
DevRhysLy <rhysproduction@gmail.com>
2021-06-26 17:17:24 +1000

    index styes fixed

--- components/ServiceList/ServiceList.js
+++ components/ServiceList/ServiceList.js
@@ -7,12 +7,14 @@ import CardBody from "components/Card/CardBody.js";
 import { cardTitle, cardLink, cardSubtitle } from "styles/jss/nextjs-material-kit.js";
 import { makeStyles } from "@material-ui/core/styles";
 import imagesStyles from "styles/jss/nextjs-material-kit/imagesStyles.js";
+import index from "styles/jss/nextjs-material-kit/pages/components.js";
 
 const styles = {
   ...imagesStyles,
   cardTitle,
   cardLink,
-  cardSubtitle
+  cardSubtitle,
+  index
 };
 
 const useStyles = makeStyles(styles);
@@ -22,24 +24,25 @@ export default function ServiceList({ services = [] }) {
   const classes = useStyles();
   return (
     <GridContainer spacing={4}>
-      {services.map((service) => (
+      {services.slice(0, 3).map((service) => (
         <GridItem xs={12} sm={6} md={4} key={service.sys.id}>
-          <Card >
-            <img
-              style={{ height: "180px", width: "100%", display: "block", objectFit: "cover" }}
-              className={classes.imgCardTop}
-              src={service.fields.image.fields.file.url}
-              alt={service.fields.service}
-            />
-            <CardBody>
-              <h4 className={classes.cardTitle}>{service.fields.service}</h4>
-              <p>{service.fields.shortDescription}</p>
-              <Link href={`/services/${service.fields.slug}`}>
-                <a>Learn More »</a>
-              </Link>
-            </CardBody>
-          </Card>
-
+          <Link href={`/services/${service.fields.slug}`}>
+            <a className={classes.serviceCardLink} >
+              <Card >
+                <img
+                  style={{ height: "180px", width: "100%", display: "block", objectFit: "cover" }}
+                  className={classes.imgCardTop}
+                  src={service.fields.image.fields.file.url}
+                  alt={service.fields.service}
+                />
+                <CardBody>
+                  <h4 className={classes.cardTitle}>{service.fields.service}</h4>
+                  <p>{service.fields.shortDescription}</p>
+                  <a>Learn More »</a>
+                </CardBody>
+              </Card>
+            </a>
+          </Link>
         </GridItem>
       ))}
     </GridContainer>
--- components/Testimonials/Testimonial.js
+++ components/Testimonials/Testimonial.js
@@ -18,8 +18,8 @@ function Testimonial({ testimonials = [] }) {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 3000,
+        autoplay: true,
+        autoplaySpeed: 3000,
       };
     return (
         <div style={{margin: "32px"}}>

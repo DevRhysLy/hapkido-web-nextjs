9a07698a221f594a5c4818fd2bf57fc642597945
RhysandLy <13537182@student.uts.edu.au>
2021-10-23 16:35:24 +1100

    error page route to home

--- pages/404.js
+++ pages/404.js
@@ -1,21 +1,12 @@
-import React from "react";
-// nodejs library that concatenates classes
-import classNames from "classnames";
-// react components for routing our app without refresh
-import Link from "next/link";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import styles from "styles/jss/nextjs-material-kit/pages/components.js";
+import React, { Component } from "react";
+import Router from "next/router";
 
-const useStyles = makeStyles(styles);
+export default class Error extends Component {
+  componentDidMount = () => {
+    Router.push("/");
+  };
 
-const Error = ()  => {
-  const classes = useStyles();
-  return (
-    <div>
-      Hello World! This is an error page!
-    </div>
-  );
+  render() {
+    return <div />;
+  }
 }
-
-export default Error;
